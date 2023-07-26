51c5a236ce2289583f09c5bdacd0100e58bafa1b
Jeeweon Hwang <ediblepotato@gmail.com>
2023-07-26 11:12:19 +0900

    2023.07.26

---
+++ B1365.py
@@ -0,0 +1,32 @@
+def lower_bound(arr, target):
+    
+    left, right = 0, len(arr)-1
+    
+    while left < right:
+        mid = left + (right - left) // 2
+        
+        if target <= arr[mid]:
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+def LIS(arr):
+
+    N = len(arr)
+    DP = [arr[0]]
+    for i in range(1, N):
+        
+        if arr[i] > DP[-1]:
+            DP.append(arr[i])
+        else:
+            idx = lower_bound(DP, arr[i])
+            DP[idx] = arr[i]
+
+    return len(DP)
+
+N = int(input())
+A = list(map(int, input().split()))
+
+lis = LIS(A)
+print(N - lis)
