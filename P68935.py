b83523815e91227e0570372bb05de0ee55af2de6
Jiwon Hwang <ediblepotato@gmail.com>
2023-08-13 22:49:27 +0900

    2023.08.13

---
+++ P68935.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    
+    result = ""
+    
+    while n:
+        result += str(n%3)
+        n //= 3
+    
+    tmp = 1
+    for digit in result[::-1]:
+        answer += int(digit) * tmp
+        tmp *= 3
+    
+    return answer
