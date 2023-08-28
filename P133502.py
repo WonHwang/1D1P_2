bdfedf140556cfbb5abdec84abc4f37ff38532e0
Jiwon Hwang <ediblepotato@gmail.com>
2023-08-28 23:09:30 +0900

    2023.08.28

---
+++ P133502.py
@@ -0,0 +1,16 @@
+def solution(ingredient):
+    answer = 0
+    stack = []
+    
+    for ing in ingredient:
+        stack.append(ing)
+        
+        if len(stack) >= 4:
+            if stack[-1] == 1 and stack[-2] == 3 and stack[-3] == 2 and stack[-4] == 1:
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                answer += 1
+            
+    return answer
