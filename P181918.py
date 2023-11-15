fc3057e35b6df0d19bbe0de76d4f330e994b5fe7
root <ediblepotato@gmail.com>
2023-11-15 08:22:55 +0000

    2023.11.15

---
+++ P181918.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while True:
+        if i >= len(arr):
+            break
+        if not stk:
+            stk.append(arr[i])
+            i += 1
+        if stk:
+            if stk[-1] < arr[i]:
+                stk.append(arr[i])
+                i += 1
+            else:
+                stk.pop()
+    return stk
