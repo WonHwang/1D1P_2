b213a5b44a436078d5f1d97e677ff7531829920f
Jiwon Hwang <ediblepotato@gmail.com>
2024-04-13 21:28:00 +0900

    2024.04.13

---
+++ P12916.java
@@ -0,0 +1,13 @@
+class Solution {
+    boolean solution(String s) {
+        int cntp = 0;
+        int cnty = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i) == 'p' || s.charAt(i) == 'P') cntp += 1;
+            if (s.charAt(i) == 'y' || s.charAt(i) == 'Y') cnty += 1;
+        }
+        
+        if (cntp == cnty) return true;
+        return false;
+    }
+}
