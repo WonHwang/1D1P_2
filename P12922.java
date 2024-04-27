d88aaef1bcce934c6d147e62c3f5831c8db25b86
Jiwon Hwang <ediblepotato@gmail.com>
2024-04-27 22:13:42 +0900

    2024.04.27

--- P12922.java
+++ P12922.java
@@ -1,6 +1,6 @@
 class P12922 {
     public String solution(int n) {
-        String answer = "수박".repeat(n/2);
+        String answer = "수박"/* .repeat(n/2) 왜 안되지*/;
         if (n%2 == 1) answer += "수";
         return answer;
     }
