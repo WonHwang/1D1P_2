3b697b5b15ede5b98c74028e09446f779fd564c2
Jiwon Hwang <ediblepotato@gmail.com>
2024-05-13 23:44:33 +0900

    2024.05.13

---
+++ P120896.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class P120896 {
+    public String solution(String s) {
+        String answer = "";
+        HashMap<Character, Integer> dict = new HashMap<>();
+        for (int i=0; i<s.length(); i++) {
+            char digit = s.charAt(i);
+            if (!dict.containsKey(digit)) dict.put(digit, 1);
+            else dict.put(digit, dict.get(digit)+1);
+        }
+        for (char c:dict.keySet()) {
+            if (dict.get(c) == 1) answer += c;
+        }
+        return answer;
+    }
+}
