36a80ddb6f998db2dcb534cb0b935e9fcf74eea6
Jiwon Hwang <ediblepotato@gmail.com>
2024-05-29 21:13:56 +0900

    2024.05.29

---
+++ P181952.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class P181952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
