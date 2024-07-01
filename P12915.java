ccef46b0b944db67f0b9b3b89730fb71916a3335
Jiwon Hwang <ediblepotato@gmail.com>
2024-07-01 22:59:41 +0900

    2024.07 .01

---
+++ P12915.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class 12915 {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String x1, String x2) {
+                if (x1.charAt(n) == x2.charAt(n)) {
+                    return x1.compareTo(x2);
+                }
+                return x1.charAt(n) - x2.charAt(n);
+            }
+        });
+        return strings;
+    }
+}
