2394a2f8a9168c417cd6fee54ea93fbbbdac1ec1
unknown <ediblepotato@gmail.com>
2024-07-28 11:53:46 +0900

    2024.07.28

---
+++ P181889.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class P181889 {
+        public int[] solution(int[] num_list, int n) {
+        int[] answer = Arrays.copyOfRange(num_list, 0, n);
+        return answer;
+    }
+}
