04a667f7e6aa80f9d41e8db050001f7e06d30759
unknown <ediblepotato@gmail.com>
2024-08-22 23:36:04 +0900

    2024.08.22

---
+++ P181907.java
@@ -0,0 +1,7 @@
+public class P181907 {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        for (int i=0; i<n; i++) answer += my_string.charAt(i);
+        return answer;
+    }
+}
