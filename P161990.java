d9dbfe629f4484703141505290bc459a4f8d7e1d
unknown <ediblepotato@gmail.com>
2025-01-18 00:06:26 +0900

    2025.01.18

---
+++ P161990.java
@@ -0,0 +1,21 @@
+public class P161990 {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = {};
+        int l = 50;
+        int t = 50;
+        int r = 0;
+        int b = 0;
+        
+        for (int i=0; i<wallpaper.length; i++) {
+            for (int j=0; j<wallpaper[0].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    if (i < t) t = i;
+                    if (i > b) b = i;
+                    if (j < l) l = j;
+                    if (j > r) r= j;
+                }
+            }
+        }
+        return new int[] {t, l, b+1, r+1};
+    }
+}
