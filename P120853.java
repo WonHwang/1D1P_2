2e97765a012b08d794394c1d2f2e3c673461f210
unknown <ediblepotato@gmail.com>
2025-02-02 23:51:59 +0900

    2025.02.02

---
+++ P120853.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class P120853 {
+    public int solution(String s) {
+        Stack<Integer> stack = new Stack<>();
+        String[] ss = s.split(" ");
+        for (String c : ss) {
+            if ("Z".equals(c)) stack.pop();
+            else stack.push(Integer.parseInt(c));
+        }
+
+        int answer = 0;
+        for (int i=0; i<stack.size(); i++) {
+            answer += stack.get(i);
+        }
+
+        return answer;
+    }
+}
