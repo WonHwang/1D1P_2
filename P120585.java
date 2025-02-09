310bcb26aa03a959556be4a614693b15e4bae368
unknown <ediblepotato@gmail.com>
2025-02-09 22:43:00 +0900

    2025.02.09

---
+++ P120585.java
@@ -0,0 +1,9 @@
+class P120585 {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for (int num : array) {
+            if (num > height) answer += 1;
+        }
+        return answer;
+    }
+}
