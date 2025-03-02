758abf2eb96fc5623c7c3cd46412e430226e7b19
unknown <ediblepotato@gmail.com>
2025-03-02 14:51:04 +0900

    2025.03.02

---
+++ P340206.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P340206 {
+        public static void main(String[] args) {
+            try (Scanner sc = new Scanner(System.in)) {
+                int angle1 = sc.nextInt();
+                int angle2 = sc.nextInt();
+                
+                int sum_angle = angle1 + angle2;
+                System.out.println(sum_angle%360);
+            }
+    }
+}
