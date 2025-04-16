e0246f4fb03d1cc63f48683a833cfdbc39b462d3
root <ediblepotato@gmail.com>
2025-04-16 08:56:31 +0000

    2025.04.16

---
+++ P120894.java
@@ -0,0 +1,11 @@
+class P120894 {
+    public long solution(String numbers) {
+        String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < 10; i++) {
+            numbers = numbers.replace(words[i], String.valueOf(i));
+        }
+        return Long.parseLong(numbers);
+        
+    }
+}
