91a7fd2bf9e38ee1f84a6f0f26d34a5c01dc970f
unknown <ediblepotato@gmail.com>
2025-05-20 23:14:00 +0900

    2025.05.20

--- B13023.py
+++ B13023.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 rel = [[] for _ in range(N)]
 for _ in range(M):
@@ -6,27 +9,28 @@ for _ in range(M):
     rel[b].append(a)
 
 answer = 0
+visited = [0] * N
 
-def dfs(visited, x):
+def dfs(x, cnt):
 
     global answer
 
     if answer:
         return
 
-    if sum(visited) == 5:
+    if cnt == 5:
         answer = 1
         return
 
     for friend in rel[x]:
         if not visited[friend]:
             visited[friend] = 1
-            dfs(visited, friend)
+            dfs(friend, cnt+1)
             visited[friend] = 0
 
 for i in range(N):
-    visited = [0] * N
     visited[i] = 1
-    dfs(visited, i)
+    dfs(i, 1)
+    visited[i] = 0
 
 print(answer)
