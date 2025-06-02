e68856c9a838c7374e2593ea0f8ac6fbd611fa16
WonHwang <43366277+WonHwang@users.noreply.github.com>
2025-06-02 07:08:45 +0000

    2025.06.02

---
+++ B24389.py
@@ -0,0 +1,21 @@
+def reverse(N):
+    res = ""
+    for digit in N:
+        if digit == "1":
+            res += "0"
+        else:
+            res += "1"
+    return res
+
+def plusone(N):
+    return bin(int("0b" + N, 2) + 1)[2:]
+
+N = bin(int(input()))[2:].zfill(32)
+M = plusone(reverse(N))
+
+answer = 0
+for i in range(32):
+    if N[i] != M[i]:
+        answer += 1
+
+print(answer)
