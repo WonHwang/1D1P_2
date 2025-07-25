59a2568d1bb9169a678b9d287a23ef2ab284096f
WonHwang <ediblepotato@gmail.com>
2025-07-25 17:03:49 +0900

    2025.07.25: P42895 resolve

--- P42895.py
+++ P42895.py
@@ -1,112 +1,46 @@
-from collections import deque
-
 def solution(N, number):
-    answer = 0
     M = 32001
-    queue = deque()
-    queue.append(N)
-    visited = [0] * M
-    visited[N] = 1
-    
-    while queue:
-        node = queue.popleft()
-        step = visited[node]
-        
-        if step > 8:
-            break
-        
-        x = int(str(N)*(step+1))
-        if x < M and not visited[x]:
-            visited[x] = step+1
-            queue.append(x)
-        
-        x = node + N
-        if x < M and not visited[x]:
-            visited[x] = step+1
-            queue.append(x)
-        
-        x = node - N
-        if 0 <= x and not visited[x]:
-            visited[x] = step+1
-            queue.append(x)
-        
-        x = node * N
-        if x < M and not visited[x]:
-            visited[x] = step+1
-            queue.append(x)
-        
-        if not node%N:
-            x = node // N
-            if 0 <= x < M and not visited[x]:
-                visited[x] = step+1
-                queue.append(x)
-    
-    dic = dict()
-    for i in range(M):
-        if visited[i]:
-            dic[i] = visited[i]
-    
-    res = visited[::]
-    for i in dic:
-        for j in dic:
-            
-            if dic[i] + dic[j] > 8:
-                continue
-            
-            num = i + j
-            if num < M and (not visited[num] or res[num] > dic[i] + dic[j]):
-                res[num] = dic[i] + dic[j]
-            
-            num = abs(i-j)
-            if not visited[num] or res[num] > dic[i] + dic[j]:
-                res[num] = dic[i] + dic[j]
-            
-            num = i*j
-            if num < M and (not visited[num] or res[num] > dic[i] + dic[j]):
-                res[num] = dic[i] + dic[j]
-            
-            if j and not i%j:
-                num = i//j
-                if not visited[num] or res[num] > dic[i] + dic[j]:
-                    res[num] = dic[i] + dic[j]
+    dp = dict()
+    for i in range(1, 9):
+        dp[int(str(N)*i)] = i
+    for _ in range(8):
+        nums = list(dp.keys())
+        for a in nums:
+            for b in nums:
+                cnt = dp[a] + dp[b]
+                if cnt < 9:
+                    num = a+b
+                    if num < M:
+                        if dp.get(num):
+                            dp[num] = min(dp.get(num), cnt)
+                        else:
+                            dp[num] = cnt
                     
-            if i and not j%i:
-                num = j//i
-                if not visited[num] or res[num] > dic[i] + dic[j]:
-                    res[num] = dic[i] + dic[j]
-    
-    dic = dict()
-    for i in range(M):
-        if res[i]:
-            dic[i] = res[i]
-    
-    result = res[::]
-    for i in dic:
-        for j in dic:
-            
-            if dic[i] + dic[j] > 8:
-                continue
-            
-            num = i + j
-            if num < M and (not res[num] or result[num] > dic[i] + dic[j]):
-                result[num] = dic[i] + dic[j]
-            
-            num = abs(i-j)
-            if not res[num] or result[num] > dic[i] + dic[j]:
-                result[num] = dic[i] + dic[j]
-            
-            num = i*j
-            if num < M and (not res[num] or result[num] > dic[i] + dic[j]):
-                result[num] = dic[i] + dic[j]
-            
-            if j and not i%j:
-                num = i//j
-                if not res[num] or result[num] > dic[i] + dic[j]:
-                    result[num] = dic[i] + dic[j]
+                    num = abs(a-b)
+                    if dp.get(num):
+                        dp[num] = min(dp.get(num), cnt)
+                    else:
+                        dp[num] = cnt
                     
-            if i and not j%i:
-                num = j//i
-                if not res[num] or result[num] > dic[i] + dic[j]:
-                    result[num] = dic[i] + dic[j]
+                    num = a*b
+                    if num < M:
+                        if dp.get(num):
+                            dp[num] = min(dp.get(num), cnt)
+                        else:
+                            dp[num] = cnt
+                    
+                    if b and not a%b:
+                        num = a//b
+                        if dp.get(num):
+                            dp[num] = min(dp.get(num), cnt)
+                        else:
+                            dp[num] = cnt
+                    
+                    if a and not b%a:
+                        num = b//a
+                        if dp.get(num):
+                            dp[num] = min(dp.get(num), cnt)
+                        else:
+                            dp[num] = cnt
     
-    return result[number] if result[number] and result[number] <= 8 else -1
+    return dp.get(number, -1)
