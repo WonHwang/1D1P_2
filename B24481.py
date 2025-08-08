5db3d992ca4b700b121e06ea9300e633bc59a5ae
WonHwang <ediblepotato@gmail.com>
2025-08-08 21:25:22 +0900

    2025.08.08

---
+++ B24481.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(node, grid, visited, step):
+
+    visited[node] = step
+
+    for x in grid[node]:
+        if not visited[x]:
+            dfs(x, grid, visited, step+1)
+
+N, M, R = map(int, input().rstrip().split())
+grid = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().rstrip().split())
+    grid[a].append(b)
+    grid[b].append(a)
+
+for i in range(1, N+1):
+    grid[i].sort()
+
+visited = [0] * (N+1)
+
+dfs(R, grid, visited, 1)
+
+for x in visited[1:]:
+    sys.stdout.write(str(x-1) + '\n')
