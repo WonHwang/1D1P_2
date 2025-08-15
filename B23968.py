3bdc1f33b882889a37f00ebf0e925848b66a19e8
WonHwang <ediblepotato@gmail.com>
2025-08-15 23:01:56 +0900

    2025.08.15

---
+++ B23968.py
@@ -0,0 +1,22 @@
+N, K = map(int, input().split())
+nums = list(map(int, input().split()))
+
+cnt = 0
+answer = []
+for last in range(N-1, 0, -1):
+    for i in range(last):
+        if nums[i] > nums[i+1]:
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+            cnt += 1
+        if cnt == K:
+            answer.append(nums[i])
+            answer.append(nums[i+1])
+            break
+
+    if answer:
+        break
+    
+if answer:
+    print(*answer)
+else:
+    print(-1)
